ef5acd45b17e9d6a0c476cfe0ccaaaa96f68880a
Joel Souza <joel.souza@gmail.com>
2011-02-23 08:01:47 -0300

    change history_state conditions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,24 @@
 $(function(){
-  if (history && history.pushState) {
-    $("input#search").keyup(function() {
-        $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
-        history.replaceState(null, document.title, $("#products_search").attr("action") + "?" + $("#products_search").serialize());
-        return false;
-    });  
+  var history_state = (history && history.pushState);
   
-    $("#products th a, #products .pagination a").live("click", function() {
-      $.getScript(this.href);
+  $("input#search").keyup(function() {
+    $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+    if(history_state)
+      history.replaceState(null, document.title, $("#products_search").attr("action") + "?" + $("#products_search").serialize());
+    return false;
+  });  
+
+  $("#products th a, #products .pagination a").live("click", function() {
+    $.getScript(this.href);
+    if(history_state)
       history.pushState(null, document.title, this.href);
-      e.preventDefault();
-      return false;
-    });
+    return false;
+  });
   
+  if(history_state){
     $(window).bind("popstate", function() {
       $.getScript(location.href);
     });
   }
+  
 })
